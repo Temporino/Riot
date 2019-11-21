fba79b3f0861db0461eaa3bffef2a8f82a82536a
Temporino <55784222+Temporino@users.noreply.github.com>
2019-11-21 12:10:51 +0100

    Create TimeSpent.py

---
+++ TimeSpent.py
@@ -0,0 +1,73 @@
+import requests
+import json
+import time
+
+api_key = "RGAPI-ca8525b8-bdf3-4155-9153-f38e633947b5"
+beginIndex=0
+gamesIds = []
+totalTime = 0
+gamesDuration = dict()
+Summoner = "Sbwinner"
+server = "euw1"
+
+def executeGet(request):
+    response = requests.get(request)
+    
+    #Rate limit
+    while(response.status_code == 429):
+        time.sleep(30)
+        response = requests.get(request)
+        
+    if response.status_code == 200:
+        return response, 1        
+    else:
+        print(response.status_code)
+        return response, None
+
+request = "https://" + server + ".api.riotgames.com/lol/summoner/v4/summoners/by-name/" + Summoner + "?api_key=" + api_key
+response, responseStatus = executeGet(request)
+if responseStatus is not None:
+    accountId = json.loads(response.content.decode('utf-8'))["accountId"]
+else:
+    print("Account could not be retrieved. Error code : " + response.status_code)
+    
+#retreive max games
+request = "https://" + server + ".api.riotgames.com/lol/match/v4/matchlists/by-account/" + accountId + "?beginIndex=" + str(100000000) + "&api_key=" + api_key
+response, responseStatus = executeGet(request)
+if responseStatus is not None:
+    maxIndex = json.loads(response.content.decode('utf-8'))["startIndex"]
+    for tempIndex in range (beginIndex, maxIndex, 100):
+        request = "https://" + server + ".api.riotgames.com/lol/match/v4/matchlists/by-account/" + accountId + "?beginIndex=" + str(tempIndex) + "&api_key=" + api_key
+        response, responseStatus = executeGet(request)
+        if responseStatus is not None:
+            games = json.loads(response.content.decode('utf-8'))["matches"]
+            for game in games:
+                gamesIds.append(game["gameId"])
+        else:
+            print("request could not be executed. Error code : " + response.status_code)
+else:
+    print("request could not be executed. Error code : " + response.status_code)
+
+def executeGet(request):
+    response = requests.get(request)
+    
+    #Rate limit
+    while(response.status_code == 429):
+        time.sleep(30)
+        response = requests.get(request)
+        
+    if response.status_code == 200:
+        return response, 1        
+    else:
+        return response, None
+
+#retrieve game duration 
+for game in gamesIds:
+    request = "https://" + server + ".api.riotgames.com/lol/match/v4/matches/"+ str(game) + "?api_key=" + api_key
+    response, responseStatus = executeGet(request)
+    if responseStatus is not None:
+        totalTime = totalTime + json.loads(response.content.decode('utf-8'))["gameDuration"]
+        gamesDuration[game] = json.loads(response.content.decode('utf-8'))["gameDuration"]
+    else:
+        print("request could not be executed. Error code : " + str(response.status_code))
+totalTime
